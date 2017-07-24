10b556eb0fa2ce5dbf4a8ea11aebfce82b161f6a
Franz Holzinger <holzinger@ttproducts.de>
2017-07-24 11:16:27 +0200

    * Initial release without any functionality

---
+++ ext_emconf.php
@@ -0,0 +1,35 @@
+<?php
+
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'External import for tt_products tables',
+	'description' => 'This contains functions to import into the tt_products tables by the extension external_import',
+	'category' => 'module',
+	'author' => 'Franz Holzinger',
+	'author_email' => 'franz@ttproducts.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => 'jambage.com',
+	'version' => '0.0.1',
+	'constraints' => array(
+		'depends' => array(
+			'div2007' => '0.8.1-0.0.0',
+			'typo3' => '7.6.0-8.7.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+);
+
